b242aec8996628d0a238dc2e66b3e2ccdf8c73ad
Junior <eecfredes@gmail.com>
2025-03-23 14:18:04 -0300

    :rocket: Get product by id controller, done

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -2,3 +2,8 @@ export function listProducts(req: any, res: any) {
   console.log(req.params);
   res.send("List of products page!");
 }
+
+export function getProductsById(req: any, res: any) {
+  console.log(req.params);
+  res.send("Just one product page!");
+}
