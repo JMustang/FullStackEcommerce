ef403571e5f11863747142f1e834d241822bf394
Junior <eecfredes@gmail.com>
2025-03-22 22:37:40 -0300

    :rocket: Products endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import productsRouter from "./routes/products/index";
 const app = express();
 const port = 3000;
 
@@ -6,28 +7,7 @@ app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 });
 
-//Products endpoints
-app.get("/products", (req: any, res: any) => {
-  console.log(req.params);
-  res.send("List of products page!");
-});
-
-app.get("/products:id", (req: any, res: any) => {
-  console.log(req.params);
-  res.send("Just one product page!");
-});
-
-app.post("/products", (req: any, res: any) => {
-  res.send("Create products page!");
-});
-
-app.put("/products:id", (req: any, res: any) => {
-  res.send("Update products page!");
-});
-
-app.delete("/products:id", (req: any, res: any) => {
-  res.send("Delete products page!");
-});
+app.use("/products", productsRouter);
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
