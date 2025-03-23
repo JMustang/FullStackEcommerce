8e2d797c5ec659fa21c5d7e5fa4db7dabf07b9c3
Junior <eecfredes@gmail.com>
2025-03-23 14:20:32 -0300

    :rocket: Create products controller, done

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -7,3 +7,7 @@ export function getProductsById(req: any, res: any) {
   console.log(req.params);
   res.send("Just one product page!");
 }
+
+export function createProducts(req: any, res: any) {
+  res.send("Create products page!");
+}
