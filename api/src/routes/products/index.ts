327ea304001e62e294c728864f3972150a97c580
Junior <eecfredes@gmail.com>
2025-03-23 14:18:35 -0300

    :rocket: Adding get product by id controller on main file

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
-import { listProducts } from "./productsController";
+import { getProductsById, listProducts } from "./productsController";
 
 //Products endpoints
 const router = Router();
 
 router.get("/", listProducts);
 
-router.get("/:id", (req: any, res: any) => {
-  console.log(req.params);
-  res.send("Just one product page!");
-});
+router.get("/:id", getProductsById);
 
 router.post("/", (req: any, res: any) => {
   res.send("Create products page!");
