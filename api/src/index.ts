6690fd30d89faa971d514f0d3946aa81cf333f23
Junior <eecfredes@gmail.com>
2025-03-22 22:13:16 -0300

    :rocket: Products endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,29 @@ app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 });
 
+//Products endpoints
+app.get("/products", (req: any, res: any) => {
+  console.log(req.params);
+  res.send("List of products page!");
+});
+
+app.get("/products:id", (req: any, res: any) => {
+  console.log(req.params);
+  res.send("Just one product page!");
+});
+
+app.post("/products", (req: any, res: any) => {
+  res.send("Create products page!");
+});
+
+app.put("/products:id", (req: any, res: any) => {
+  res.send("Update products page!");
+});
+
+app.delete("/products:id", (req: any, res: any) => {
+  res.send("Delete products page!");
+});
+
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
 });
