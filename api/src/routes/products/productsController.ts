1a6e18a90ff630e237a2f548c5289b82a4c39314
Junior <eecfredes@gmail.com>
2025-03-29 22:02:08 -0300

    :rocket: CRUD. List all products, done

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -2,14 +2,20 @@ import { Request, Response } from "express";
 import { db } from "../../db/index";
 import { productsTable } from "../../db/productsSchema";
 
-export function listProducts(req: Request, res: Response) {
-  console.log(req.params);
-  res.send("List of products page!");
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await db.select().from(productsTable);
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
 export function getProductsById(req: Request, res: Response) {
-  console.log(req.params);
-  res.send("Just one product page!");
+  try {
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
 export async function createProducts(req: Request, res: Response) {
@@ -25,9 +31,15 @@ export async function createProducts(req: Request, res: Response) {
 }
 
 export function updateProducts(req: Request, res: Response) {
-  res.send("Update products page!");
+  try {
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
 export function deleteProducts(req: Request, res: Response) {
-  res.send("Delete products page!");
+  try {
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
